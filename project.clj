e34e57041086497ad64e22225dfbcfd5ec79a553
Christopher Brown <io@henrian.com>
2017-06-11 18:54:23 -0500

    Set project config to target clojars for releases

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/chbrown/jurl-clojure"
   :license {:name "Eclipse Public License"
             :url "https://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.7.0"]])

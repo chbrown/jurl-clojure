eb9b3e3eb665ff9e4e63393dc81fe049e345c46b
Christopher Brown <io@henrian.com>
2017-06-13 00:32:28 -0500

    Add Travis CI with coverage support via Coveralls

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "https://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :profiles {:test {:plugins [[lein-cloverage "1.0.9"]]}})

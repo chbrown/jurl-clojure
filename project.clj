6444c73b05878b01f5484832c3e1bde0a4043da4
Christopher Brown <io@henrian.com>
2017-06-14 02:43:24 -0500

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jurl "0.1.1"
+(defproject jurl "0.1.2-SNAPSHOT"
   :description "Cross-platform URL parsing / manipulation"
   :url "https://github.com/chbrown/jurl-clojure"
   :license {:name "Eclipse Public License"

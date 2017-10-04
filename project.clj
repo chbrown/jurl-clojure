a9506d2c657e20e80db2a599a8878abc0c353336
Christopher Brown <io@henrian.com>
2017-10-04 17:55:33 -0500

    Update to newest doo and remove shim workaround

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.908"]]
+                 [org.clojure/clojurescript "1.9.946"]]
   :plugins [[lein-cljsbuild "1.1.7"]]
   :cljsbuild {:builds [{:id "production"
                         :source-paths ["src"]
@@ -17,8 +17,7 @@
                         :compiler {:output-dir "target/test"
                                    :output-to "target/test/main.js"
                                    :main jurl.runner
-                                   :process-shim false ; remove when https://git.io/v53Vh is closed
                                    :optimizations :whitespace}}]}
   :profiles {:test {:doo {:paths {:rhino "lein run -m org.mozilla.javascript.tools.shell.Main"}}
-                    :plugins [[lein-doo "0.1.7"]
+                    :plugins [[lein-doo "0.1.8"]
                               [lein-cloverage "1.0.9"]]}})
